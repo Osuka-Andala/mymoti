b7fb204a67cddbf20b044cc77a275897ad45187e
Osuka-Andala <teresa.andala@gmail.com>
2015-09-21 20:08:20 +0300

    Added new java class

---
+++ app/src/main/java/Splash.java
@@ -0,0 +1,13 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by osuka on 9/20/15.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
